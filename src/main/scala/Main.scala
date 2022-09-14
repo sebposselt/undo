5f6bb03b23ce28cd76ca6d617ee931d42001744e
Sebastian Posselt <posselt_123@hotmail.com>
2022-09-14 20:10:25 +0200

    result now shows previous word also. this is for debugging

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -14,49 +14,65 @@ object Application {
       println("start word and end word must be same length")
     } 
     else {
-     // implicit workset is to avoid threading the workset through every subsequent function call. essentially creating a state monad
-     implicit val workSet:List[String] = createWorkSet(dictionaryPath, startWord.length()) //set is not the matmatical meaning
-     val res = wordChain(Set(startWord), endWord, List(), workSet)
+     // implicit workDict is to avoid threading the workDict through every subsequent function call. essentially creating a state monad
+     implicit val workDict:List[String] = createworkDict(dictionaryPath, startWord.length()) //set is not the matmatical meaning
+     val res = wordChain(Set(startWord), "", endWord, List(), workDict)
      
      println(s"\ndone!\nThe length of the chain is: ${res.length}\nThe chain is:\n")
-     for {w <- res.reverse} println(w)
+     for {l <- res.reverse} println(l)
     }
   }
 
-  def wordChain (currentWords:Set[String], endWord:String, acc:List[String], workSet:List[String]): List[String] = {
-    println(currentWords.count(s=>true))
-    val w = currentWords.head
-    val tail = currentWords.tail
-    val newAcc = w :: acc
-    val nextWords = oneStep((w), workSet).toSet
-    
+  def wordChain (wordQueue:Set[String], prevStep:String, endWord:String, acc:List[List[String]],
+   workDict:List[String]): List[List[String]] = {
+    val w = wordQueue.head
+    val newAcc = List(prevStep, w) :: acc
+    val nextWords = oneStep((w), workDict).toSet
+    // println()
+    // println(s"new call. head: ${w}")
+    // println(s"newAcc: ${newAcc}")
+    // println(s"tail: ${wordQueue.tail}")
+    // println(s"newSteps: ${nextWords}")
+    // println()
     if (nextWords.contains(endWord)){
-      endWord::newAcc
+      List(w,endWord) :: newAcc
     }
     else {
-      val newCurWords = (nextWords union tail) -- newAcc.toSet
-      
-      wordChain(newCurWords, endWord, newAcc, workSet)
+      val newCurWords = (nextWords union wordQueue.tail) -- newAcc.flatten.toSet  //important to remove already tried steps as it can otherwise flip the same letter back and forth
+      wordChain(newCurWords, w, endWord, newAcc, workDict)
+    
     }
   }
 
-  def flipLetter (word:String, flipIdx:Int, workset:List[String]) :List[String] = {
+  def flipLetter (word:String, flipIdx:Int, workDict:List[String]) :List[String] = {
     val alphabet = List('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
     val flipChar = (w:String, i:Int, c:Char) => w.substring(0,i) + c + w.substring(i+1)
     val f = (c:Char) => if (c == word(flipIdx)) word else flipChar(word, flipIdx, c)
     val lst = alphabet.map(f)
-    lst.filter((w:String) => w != word && workset.contains(w) )
+    lst.filter((w:String) => w != word && workDict.contains(w) )
   }
 
-  def oneStep(word:String, workset:List[String]) :List[String] = {
+  def oneStep(word:String, workDict:List[String]) :List[String] = {
     val idxs:List[Int] = (for {i <- 0 to word.length -1 } yield i).toList
-    idxs.flatMap(flipLetter(word, _, workset))
+    idxs.flatMap(flipLetter(word, _, workDict))
   }
 
   // function to trim the dictionary to relevent words in efforts to speed up lookups.
-  def createWorkSet(filename:String, wordLength:Int): List[String] = {
+  def createworkDict(filename:String, wordLength:Int): List[String] = {
     val dict = Source.fromFile(filename).getLines.toList
     dict.filter( (x:String) => x.length == wordLength )
   }
 
 }
+
+
+
+
+
+/*    NOTES: 
+Im trying to do a BFS, but its important to improve speed and complexity 
+that the set of next words to take a step from does not contain dulicate 
+and previous steps as this would essentially create a cycle in the search 
+tree and can cause an infinite loop
+
+*/
